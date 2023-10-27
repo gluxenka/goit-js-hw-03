3b783b782eb6eddaebf2e2314015ff2a385a8d78
gluxenka <gluxenka750@gmail.com>
2023-10-27 15:28:22 +0300

    done

---
+++ task-1.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @param {string} title
+ * @returns
+ */
+// function slugify(title) {
+//   return title.toLowerCase().replaceAll(" ", "-");
+// }
+
+function slugify(title) {
+  const titleLower = title.toLowerCase();
+  let slug = "";
+  for (let index = 0; index < title.length; index++) {
+    const element = titleLower[index];
+    if (element === " ") {
+      slug += "-";
+    } else {
+      slug += element;
+    }
+  }
+  return slug;
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
